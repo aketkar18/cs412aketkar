e92adc2826a38da26d66ba64cd465aba5182aff1
aketkar18 <anujketkar1118@gmail.com>
2023-11-24 00:21:05 -0600

    Added website files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personal Website
+
+This is the repository for my [personal website](www.anujketkar.com) using only HTML and CSS.
---
+++ functions.js
@@ -0,0 +1,8 @@
+function menu() {
+    var x = document.getElementById("myNavi");
+    if (x.className === "navi") {
+      x.className += " responsive";
+    } else {
+      x.className = "navi";
+    }
+  }
